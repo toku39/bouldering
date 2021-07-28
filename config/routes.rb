f35676b2a84e8949330e64caea03157ba12ae1b9
toku39 <86103018+toku39@users.noreply.github.com>
2021-07-28 14:48:50 +0900

    投稿詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "problems#index"
-  resources :problems, only: [:index, :new, :create] 
+  resources :problems, only: [:index, :new, :create, :show] 
 end
